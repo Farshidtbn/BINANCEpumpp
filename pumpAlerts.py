f54c42d515d4e340052922476c879e43f1de85c8
Farshidtbn <159628664+Farshidtbn@users.noreply.github.com>
2024-06-10 01:18:00 +0330

    Update pumpAlerts.py

--- pumpAlerts.py
+++ pumpAlerts.py
@@ -44,7 +44,7 @@ logger.debug("Config: %s", config)
 
 def main():
     telegram = TelegramSender(
-        token=config["7290245691:AAHpzaWQFogiDldc6jSEHDoo8h4-MeeSiXw"],
+        token=config["AAHpzaWQFogiDldc6jSEHDoo8h4-MeeSiXw"],
         chat_id=config["-1002135857346"],
         alert_chat_id=config["-1002135857346"]
         if "-1002135857346" in config and config["-1002135857346"] != 0
